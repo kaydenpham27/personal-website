0274029984d7f83e423638b3804f83d23c9cac20
KienPham <kienpt5kkd@gmail.com>
2025-10-26 12:43:52 +1000

    Add GitHub component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,77 @@
-import type React from "react";
+import React from "react";
+
+type GitHubRepoData = {
+  forks_count: number;
+  full_name: string;
+  stargazers_count: number;
+  html_url: string;
+};
+
+type GitHubStatsProps = {
+  ownerName: string;
+  repoName: string;
+};
+
+const GitHubStats = (props: GitHubStatsProps) => {
+  const [repoData, setRepoData] = React.useState<GitHubRepoData | null>(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch(`https://api.github.com/repos/${props.ownerName}/${props.repoName}`)
+      .then((res) => res.json())
+      .then((data: GitHubRepoData) => {
+        setRepoData(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(
+          `Failed to fetch GitHub repository ${props.repoName}: ${err}`,
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="flex flex-row">
+      <a
+        href={repoData?.html_url}
+        title="Go to repository"
+        className="github-repo"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="github-repo__icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path d="M173.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6m-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3m44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9M252.8 8C114.1 8 8 113.3 8 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C436.2 457.8 504 362.9 504 252 504 113.3 391.5 8 252.8 8M105.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1m-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7m32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1m-11.4-14.7c-1.6 1-1.6 3.6 0 5.9s4.3 3.3 5.6 2.3c1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2" />
+          </svg>
+        </div>
+      </a>
+      <div className="github-repo__content">
+        <div className="github-repo__name">{repoData?.full_name}</div>
+        <ul className="github-repo__stats">
+          <li className="github-repo__stat github-repo__stat--stars">
+            {repoData?.stargazers_count}
+          </li>
+          <li className="github-repo__stat github-repo__stat--forks">
+            {repoData?.forks_count}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export const SideBar = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <div className="flex flex-col w-full h-20 place-content-center">
+      {/* Header */}
+      <div className="grid grid-cols-1 md:grid-cols-[70%_30%]">
+        <div>icon</div>
+        <GitHubStats repoName="personal-website" ownerName="kaydenpham27" />
+      </div>
+      {children}
+    </div>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   base: "/personal-website/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
