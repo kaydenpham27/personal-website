1685fc925494b1f7486968f5b5720192f005ad9d
Kien Pham <kienpt5kkd@gmail.com>
2025-10-27 23:20:55 +1000

    Add LinkedIn component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -41,11 +41,11 @@ const GitHubStats = (props: GitHubStatsProps) => {
     <a
       href={repoData?.html_url}
       title="Go to repository"
-      className="flex flex-row space-x-2"
+      className="flex flex-row space-x-2 justify-center"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="github-repo__icon">
+      <div>
         <svg
           width="40px"
           height="40px"
@@ -114,13 +114,39 @@ const GitHubStats = (props: GitHubStatsProps) => {
   );
 };
 
+const LinkedIn = () => {
+  return (
+    <a
+      href="https://www.linkedin.com/in/kienpham04/"
+      title="Connect me!"
+      target="_blank"
+    >
+      <svg
+        width="50px"
+        height="50px"
+        viewBox="0 0 16 16"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+      >
+        <path
+          fill="#0A66C2"
+          d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z"
+        />
+      </svg>
+    </a>
+  );
+};
+
 export const SideBar = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col w-full h-20 place-content-center">
+    <div className="flex flex-col w-full h-17 place-content-center">
       {/* Header */}
-      <div className="grid grid-cols-1 md:grid-cols-[70%_30%]">
-        <div>icon</div>
-        <GitHubStats repoName="personal-website" ownerName="kaydenpham27" />
+      <div className="grid grid-cols-2 md:grid-cols-[70%_30%]">
+        <div className="">icon</div>
+        <div className="flex flex-row space-x-10">
+          <GitHubStats repoName="personal-website" ownerName="kaydenpham27" />
+          <LinkedIn />
+        </div>
       </div>
       {children}
     </div>
