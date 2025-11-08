7ae35932d77847e35deaf622649f805239897a3d
Kien Pham <kienpt5kkd@gmail.com>
2025-11-08 20:59:39 +1000

    Add personal details

--- src/pages/HomePage/components/InfoCard.tsx
+++ src/pages/HomePage/components/InfoCard.tsx
@@ -16,17 +16,17 @@ export const InfoCard = ({}: InfoCardProps) => {
   return (
     <div className="w-full">
       <Card className="w-full">
-        {/* <CardHeader>
-        <CardTitle>
-          <Typography.H1></Typography.H1>
-        </CardTitle>
-        <CardDescription>
-          <Typography.Muted>This is my website</Typography.Muted>
-        </CardDescription>
-      </CardHeader> */}
-        <CardContent className="grid grid-cols-1 md:grid-cols-[70%_30%] gap-4">
+        <CardHeader>
+          <CardTitle>
+            <Typography.H1> About Me </Typography.H1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="grid grid-cols-1 md:grid-cols-[65%_35%] gap-4">
           <PersonalDetailsCard />
-          <div>Picture</div>
+          <img
+            src="./public/AVA.png"
+            className="w-60 place-self-center rounded-xs border-solid border-2 border-gray-700"
+          />
         </CardContent>
       </Card>
     </div>
--- src/pages/HomePage/components/PersonalDetailsCard.tsx
+++ src/pages/HomePage/components/PersonalDetailsCard.tsx
@@ -1,15 +1,20 @@
 import Typography from "@/components/ui/typography";
 
 type DetailsRowProps = {
+  iconUrl: string;
   title: string;
   description: string;
 };
 
-const DetailsRow = ({ title, description }: DetailsRowProps) => {
+const DetailsRow = ({ iconUrl, title, description }: DetailsRowProps) => {
   return (
-    <div className="flex flex-row justify-items-center items-center pt-4">
-      <Typography.H4> {title} </Typography.H4>
-      <Typography.Medium className="pl-4"> {description} </Typography.Medium>
+    <div className="w-full grid grid-cols-3  md:grid-cols-[5%_20%_75%] items-center pt-4">
+      <img src={iconUrl} className="w-8" />
+      <Typography.H4 className="pl-4"> {title} </Typography.H4>
+      <Typography.Medium className="pl-4 text-left">
+        {" "}
+        {description}{" "}
+      </Typography.Medium>
     </div>
   );
 };
@@ -21,10 +26,30 @@ export const PersonalDetailsCard = () => {
       <Typography.Large className="font-normal">
         Trung Kien Pham
       </Typography.Large>
-      <Typography.Medium> Brisbane, Australia </Typography.Medium>
-      <DetailsRow title="Fav drink" description="Iced long black" />
-      <DetailsRow title="Fav drink" description="Iced long black" />
-      <DetailsRow title="Fav drink" description="Iced long black" />
+      <Typography.Medium className="pb-6">
+        {" "}
+        Brisbane, Australia{" "}
+      </Typography.Medium>
+      <DetailsRow
+        iconUrl="./public/work_place.png"
+        title="Company"
+        description="Nanoputian"
+      />
+      <DetailsRow
+        iconUrl="./public/energy_drink.png"
+        title="Drink"
+        description="I love iced long black, don't know if 
+        this is what they call it anywhere else, but in Australia, this is a GOATed drink. I can
+        survive only by drinking iced long blacks. An iced long black can boost my software development
+        productivity by INF. 
+        "
+      />
+      <DetailsRow
+        iconUrl="./public/enjoy.png"
+        title="Hobbies"
+        description="I love doing Competive Programming where I have ... 
+        "
+      />
     </div>
   );
 };
