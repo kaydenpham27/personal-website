6b00fbba4ddf167fea007989e2142b83d986be25
Kien Pham <kienpt5kkd@gmail.com>
2025-11-09 01:13:34 +1000

    Add basename for browser router

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,14 +1,15 @@
 import { SideBar } from "@/components/SideBar";
+import { BlogsPage } from "@/pages/BlogsPage";
 import { HomePage } from "@/pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="personal-website/">
       <SideBar>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/posts" element={<HomePage />} />
+          <Route path="/posts" element={<BlogsPage />} />
           <Route path="/contact" element={<HomePage />} />
           <Route path="*" element={<HomePage />} />
         </Routes>
